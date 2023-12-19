5c2753657ff5068370e73c18c597ada8324f1776
jis3r <jis3r@protonmail.com>
2023-12-20 00:09:17 +0100

    add: tailwind setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="bg-neutral-950 h-screen w-screen">
+	<div class="flex flex-col items-center justify-center h-full">
+		<h1 class="text-4xl text-white">Gradient from Image</h1>
+		<p class="text-white">This is a boilerplate</p>
+	</div>
+</main>
